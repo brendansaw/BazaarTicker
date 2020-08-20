1fead654a5617cc8a06b6448f010959ef16f0680
brendansaw <brendansaw@gmail.com>
2020-08-20 13:22:25 -0700

    Added API call

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 // main.js
+
+bazaarLink = 'https://api.hypixel.net/skyblock/bazaar';
+$.getJSON(bazaarLink, function(data) {
+    outputText = "Success value: " + data.success + "<br>lastUpdated value: " + data.lastUpdated; 
+    $(".interface").html(outputText);
+});
