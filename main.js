468bf1d62a48902d137d77062196250fb2ab7d7f
brendansaw <brendansaw@gmail.com>
2020-08-20 14:27:26 -0700

    Updated main.js

--- main.js
+++ main.js
@@ -2,6 +2,23 @@
 
 bazaarLink = 'https://api.hypixel.net/skyblock/bazaar';
 $.getJSON(bazaarLink, function(data) {
-    outputText = "Success value: " + data.success + "<br>lastUpdated value: " + data.lastUpdated; 
-    $(".interface").html(outputText);
+    //outputText = "Success value: " + data.success + "<br>lastUpdated value: " + data.lastUpdated; 
+    //inside = data.products.BROWN_MUSHROOM.quick_status.productId;
+    products = data.products;
+
+    itemList = [];
+    itemBuyPrice = [];
+    itemSellPrice = [];
+    for (item in products) {
+        itemList.push(products[item].quick_status.productId);
+        itemBuyPrice.push(products[item].quick_status.buyPrice);
+        itemSellPrice.push(products[item].quick_status.sellPrice);
+    }
+
+    output = "";
+    for (i = 0; i < itemList.length; i++) {
+        output += itemList[i] + "   Buy Price: " + itemBuyPrice[i] + "   Sell Price: " + itemSellPrice[i] + "<br>";
+    }
+
+    $(".interface").html(output);
 });
