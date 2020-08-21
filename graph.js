77068cd0b2301f6972a82b6a6c870a383212c729
Roy Li <roygagali@gmail.com>
2020-08-20 17:47:04 -0700

    cleaned up graph.js

--- graph.js
+++ graph.js
@@ -1,14 +1,36 @@
-x = [];
-y = [];
-
-// main.js
-
+buyData = [];
+sellData = [];
 bazaarLink = 'https://api.hypixel.net/skyblock/bazaar';
+var ctx = document.getElementById('itemStatGraph').getContext('2d');
 
+// creating charts
+var itemStatGraph = new Chart(ctx, {
+    type: 'line',
+    data: {
+            labels: [],
+            datasets: [{
+                label: 'Buy Price',
+                backgroundColor: 'rgb(255, 0, 0)',
+                fill: false,
+                borderColor: 'rgb(255, 0, 0)',
+                data: buyData
+            },
+
+            {
+                label: 'Sell Price',
+                backgroundColor: 'rgb(0, 255, 0)',
+                fill: false,
+                borderColor: 'rgb(0, 255, 0)',
+                data: sellData
+            }]
+        },
+
+    options: {}
+});
+
+// colllecting stat data
 function getStats(){
     $.getJSON(bazaarLink, function(data) {
-        //outputText = "Success value: " + data.success + "<br>lastUpdated value: " + data.lastUpdated; 
-        //inside = data.products.BROWN_MUSHROOM.quick_status.productId;
         products = data.products;
     
         itemList = [];
@@ -27,46 +49,23 @@ function getStats(){
     return [itemBuyPrice, itemSellPrice];
 }
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            datasets: [{
-                label: 'Buy Price',
-                backgroundColor: 'rgb(255, 0, 0)',
-                fill: false,
-                borderColor: 'rgb(255, 0, 0)',
-                data: x
-            },
-
-            {
-                label: 'Sell Price',
-                backgroundColor: 'rgb(0, 255, 0)',
-                fill: false,
-                borderColor: 'rgb(0, 255, 0)',
-                data: y
-            }]
-        },
-
-    options: {}
-});
-
-d = 0;
-
-function update(){
+// update graph on screen
+function updateGraph(){
     stats = getStats();
     itemBuyPrice = stats[0];
     itemSellPrice = stats[1];
 
-    i = itemBuyPrice[itemBuyPrice.length-1];
-    i2 = itemSellPrice[itemSellPrice.length-1];
+    // TODO give input to update graph function
+    index = itemBuyPrice.length-1
+    currBuy = itemBuyPrice[index];
+    currSell = itemSellPrice[index];
 
-    y.push(i2);
-    x.push(i);
-    myChart.data.labels.push('boi');     
-    myChart.update();    
+    sellData.push(currBuy);
+    buyData.push(currSell);   
+    itemStatGraph.data.labels.push('boi');  
+    itemStatGraph.update();    
 }
 
 
-window.setInterval(update, 5000);
+//call update function every 5s
+window.setInterval(updateGraph, 5000);
