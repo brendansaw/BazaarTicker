02d45cb974b4153c1c06433bd0178518a8934f35
brendansaw <brendansaw@gmail.com>
2020-08-27 16:07:26 -0700

    Experimenting with getting data from PHP

--- callFromServer.js
+++ callFromServer.js
@@ -2,7 +2,7 @@ $.ajax({
     url : 'getDataFromServer.php', // your php file
     type : 'GET', // type of the HTTP request
     success : function(data){
-       var obj = data;
+       var obj = JSON.parse(data);
        console.log(obj);
     }
  });
--- getDataFromServer.php
+++ getDataFromServer.php
@@ -1,9 +1,9 @@
 <?php
     $mysqli = new mysqli('localhost:3306', 'root', 'root', 'test');
-    $q = "SELECT * FROM `bazaarsecond` LIMIT 0,1";
-	$res = $sqlHandler -> query($q);
-    echo $res -> fetch_row();
+    $q = "SELECT * FROM 'bazaarsecond' LIMIT 0,1";
+	$res = $mysqli -> query($q);
+    $d = $res -> fetch_row();
     
-
+    echo json_encode($d[0]);
 
 ?>
