5990426cc473fffce631a4760b8ec0dfc016dd9f
Roy Li <roygagali@gmail.com>
2020-08-31 14:14:01 -0700

    deleted callfromserver, graphtest

--- callFromServer.js
+++
@@ -1,8 +0,0 @@
-$.ajax({
-    url : 'getDataFromServer.php', // your php file
-    type : 'GET', // type of the HTTP request
-    success : function(data){
-       var obj = data;
-       console.log(obj);
-    }
- });
