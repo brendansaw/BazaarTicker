d1d3c1ded0f1be36eb16e379e209f6bca82e1ee8
MarkusH17 <markushughes1733@gmail.com>
2020-09-01 11:17:41 -0700

    uploading

--- formtoJSON.php
+++ formtoJSON.php
@@ -1,10 +1,5 @@
 <?php
-/*FORMAT
-Object.keys(Mailer1.buy).length + ... > 3 then dont allow it
 
-whenever the price is greter or les then depend on buy or sell
-sendMail()r
-*/
 
     $jsonString = file_get_contents('emailList.json');
     $data = json_decode($jsonString);
@@ -12,7 +7,7 @@ sendMail()r
 
     $data['Mailer1']['buy']['EMERALD'] = 10000;
 
-    $newJson = json_encode($data);
+    $newJson = json_encode($data); 
     file_put_contents('emailList.json', $newJson);
 
 ?>
