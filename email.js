e23bbd2794b6ed769aeaae74345a987f1f74cc6a
brendansaw <brendansaw@gmail.com>
2020-09-05 21:47:54 -0700

    Added write to MySQL and sweep through MySQL

--- email.js
+++ email.js
@@ -1,77 +1,76 @@
-$(function() {
+$('.error').hide();
+$(".dataButtonClass").click(function() {
+    event.preventDefault();
     $('.error').hide();
-    $(".dataButtonClass").click(function() {
+    var error = 0;
+    var uemail = $("input#uemail").val();  
+    uemail = escape_html(uemail);
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var test_1 = uemail.match(mailformat);
+    if (!test_1) {
+        $("label#uemail_error").show();
+        $("input#uemail").focus();
+        ++error;    
+    }
+    
+    var formItem = $("input#formItem").val();
+    if (formItem == "") {
+        $("label#formItem_error").show();
+        $("input#formItem").focus();
+        ++error;
+    }
+    var bors = document.getElementsByName("buyorsell");
+    if (bors[0].checked == false && bors[1].checked == false) {
+        $("label#buyorsell_error").show();
+        $("input#buyorsell").focus();
+        ++error;
+    }
+    var priceReq = $("input#priceReq").val();
+    invalidPrice = document.getElementsByName("priceReq");
+    if (priceReq == "") {
+        invalidPrice.innerHTML == "Please specify a price"
+        $("label#priceReq_error").show();
+        $("input#priceReq").focus();
+        ++error;
+    } 
+    else if (priceReq < 0) {
+        $("label#priceReq_error").show();
+        $("input#priceReq").focus();
+        ++error;
+    }
 
-        $('.error').hide();
-        var error = 0;
-            var uemail = $("input#uemail").val();  
-            uemail = escape_html(uemail);
-            var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-            var test_1 = uemail.match(mailformat);
-            if (!test_1) {
-                $("label#uemail_error").show();
-                $("input#uemail").focus();
-                ++error;    
+    if (error > 0) {
+        return false;
+    }
+    else {
+        radio = document.getElementsByName("buyorsell");
+        buyorsellRadio = ""
+        for (i = 0; i < radio.length; ++i) {
+            if(radio[i].checked) {
+                buyorsellRadio = radio[i].value;
             }
-            
-            var formItem = $("input#formItem").val();
-            if (formItem == "") {
-                $("label#formItem_error").show();
-                $("input#formItem").focus();
-                ++error;
-            }
-            var bors = document.getElementsByName("buyorsell");
-            if (bors[0].checked == false && bors[1].checked == false) {
-                $("label#buyorsell_error").show();
-                $("input#buyorsell").focus();
-                ++error;
-            }
-            var priceReq = $("input#priceReq").val();
-            invalidPrice = document.getElementsByName("priceReq");
-            if (priceReq == "") {
-                invalidPrice.innerHTML == "Please specify a price"
-                $("label#priceReq_error").show();
-                $("input#priceReq").focus();
-                ++error;
-            } 
-            else if (priceReq < 0) {
-                $("label#priceReq_error").show();
-                $("input#priceReq").focus();
-                ++error;
-            }
-
-        if (error > 0) {
-            return false;
         }
-        else {
-            radio = document.getElementsByName("buyorsell");
-            bruh = ""
-            for (i = 0; i < radio.length; ++i) {
-                if(radio[i].checked) {
-                    bruh = radio[i].value;
-                }
-            }
-            var formData = {
-                'email' : $('input#uemail').val(),
-                'item' : $('input#formItem').val(),
-                'buyorsell' : bruh,
-                'price' : $('input#priceReq').val()
-            };
 
-            $.ajax({
-                type: 'POST',
-                url: 'formtoJSON.php',
-                data: formData,
-                dataType: 'json'
-                 
-            })
-            
-            return false;
-        }
-    });
+        var obj = new Object();
+        obj.uemail = $('input#uemail').val();
+        obj.item = $('input#formItem').val();
+        obj.buyorsell = buyorsellRadio;
+        obj.priceReq = $('input#priceReq').val();
+
+        $.ajax({
+            type: 'POST',
+            url: 'formToServer.php',
+            data: obj,
+            success: function(response)
+            {
+                jsonData = JSON.parse(response);
+                console.log(jsonData);
+            }
+        })
+        
+        return false;
+    }
 });
-
-   
 
 function escape_html(str) {
   
@@ -87,26 +86,3 @@ function escape_html(str) {
     return str.replace(/[&<>"']/g, function(m) { return map[m]; });
 }
 
-function sendMail() {
-
-    buy00 = document.getElementById("buy00");
-    buy10 = document.getElementById("buy10");
-    buy20 = document.getElementById("buy20");
-    sell00 = document.getElementById("sell00");
-    sell10 = document.getElementById("sell10");
-    sell20 = document.getElementById("sell20");
-
-    bodystr = "Item: " + formItem.value + "<br> Best Buy Price: " + buy00.innerHTML + "<br> Quantity: " + buy10.innerHTML + "<br> Orders: " + buy20.innerHTML + "<br> Best Sell Price: " + sell20.innerHTML + "<br> Quantity: " + sell10.innerHTML + "<br> Orders: " + sell00.innerHTML + "<br> Thank you for using our services!";
-
-    Email.send({
-        Host: "smtp.gmail.com",
-        Username: "bazaarticker@gmail.com",
-        Password: "o*L7GJ08MmrXO7MT",
-        To : uemail.value,
-        From: "bazaarticker@gmail.com",
-        Subject: "BazaarTracker Data",
-        Body: bodystr,
-    }).then(
-        message => alert("Data sent successfully")
-    );
-}
